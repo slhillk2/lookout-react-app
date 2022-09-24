c565cd4b9f23644dfa0acdeaf7d6a1ce16943f26
slhillk2 <stephenhill@stephens-MacBook-Pro.local>
2022-09-24 16:34:45 +0100

    add bootstrap nav bar to header component / import logo image

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,33 @@
+import main_logo from '../assets/logo/main_logo.png'
+
 const Header = ({ title }) => {
   return (
-    <header>
-        <h1>{title}</h1>
-    </header>
+    <nav className="navbar navbar-expand-lg navbar-custom">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      
+      <div class="navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav mx-auto nav-pills">
+          <li className='nav-item active'> <a className='nav-link'>Home</a></li>
+          <li className='nav-item'> <a className='nav-link'>Gallery</a></li>
+
+          <a>
+          <img src={main_logo} id='logo' alt="lookout logo" />
+          </a>
+
+          <li className='nav-item'> <a className='nav-link'>Contact</a></li>
+          <li className='nav-item'> <a className='nav-link'>Menu</a></li>
+        </ul>
+      </div>
+      
+      <div class="nav-collapse">
+        <ul class="nav">
+          
+        </ul>
+      </div>
+      
+    </nav>
   )
 }
 
